a1799e27e63186b1a85e042dc58c52f2e206b5a6
Steven Swor <sworisbreathing@gmail.com>
2015-05-12 22:44:04 +1000

    configure forwarder as deployment client

--- ansible_templates.tf
+++ ansible_templates.tf
@@ -3,6 +3,7 @@ resource "template_file" "ansible_inventory" {
   vars {
     tlpinfmgt03vth = "${aws_instance.tlpinfmgt03vth.public_ip}"
     tldhybqat01vcl = "${aws_instance.tldhybqat01vcl.public_ip}"
+    deployment_server_host = "${aws_instance.tlpinfmgt03vth.private_ip}"
   }
 }
 output "ansible_inventory_rendered" {
--- templates/ansible_inventory.tpl
+++ templates/ansible_inventory.tpl
@@ -1,5 +1,8 @@
 [splunk_forwarders]
 tldhybqat01vcl ansible_ssh_user=ubuntu ansible_ssh_host=${tldhybqat01vcl}
 
+[splunk_forwarders:vars]
+splunk_forwarder_deployment_server_host=${deployment_server_host}
+
 [splunk_servers]
 tlpinfmgt03vth ansible_ssh_user=ubuntu ansible_ssh_host=${tlpinfmgt03vth}
