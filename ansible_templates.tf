00ca3b7e09b67a98ed60c853c40f190daf5189a5
Steven Swor <sworisbreathing@gmail.com>
2015-05-12 17:41:41 +1000

    splunk 5.0.2 server

--- ansible_templates.tf
+++ ansible_templates.tf
@@ -1,6 +1,7 @@
 resource "template_file" "ansible_inventory" {
   filename = "templates/ansible_inventory.tpl"
   vars {
+    tlpinfmgt03vth = "${aws_instance.tlpinfmgt03vth.public_ip}"
     tldhybqat01vcl = "${aws_instance.tldhybqat01vcl.public_ip}"
   }
 }
